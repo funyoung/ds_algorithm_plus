e2ce22a2fd743ffb8fc7f7b528d0d6117cf563f9
Yang Feng <e13310@gmail.com>
2022-07-19 16:07:31 +0800

    Implement insert sort with go

--- go/hello_world.go
+++ go/hello_world.go
@@ -2,7 +2,22 @@ package main
 
 import "fmt"
 
+/**
+  * 插入排序，移动插入版
+  */
 func insertSort(arr []int, n int) []int {
+    for i := 1; i < n; i++ {
+        tmp := arr[i]
+        j := i - 1
+        for ; j >= 0; j-- {
+            if arr[j] > tmp {
+                arr[j + 1] = arr[j]
+            } else {
+                break
+            }
+        }
+        arr[j + 1] = tmp
+    }
     return arr
 }
 
@@ -12,6 +27,14 @@ func printArray(arr []int, n int) {
     }
 }
 
+func assertArrayEquals(arr []int, expect []int, n int) bool {
+    for i := 0; i < n; i++ {
+        if expect[i] != arr[i] {
+            return false
+        }
+    }
+    return true
+}
 /**
   * tasks:
   * declare an array to be sorted fill with some int
@@ -24,8 +47,14 @@ func main() {
     fmt.Println("Insert Sort begin")
     a := []int{45, 34, 78, 12, 34, 32, 29, 64}
     b := []int{12, 29, 32, 34, 34, 45, 64, 78}
+    //printArray(a, len(a))
     insertSort(a, len(a))
-    printArray(a, len(a))
-    printArray(b, len(b))
+    //printArray(a, len(a))
+    //printArray(b, len(b))
+    if assertArrayEquals(a, b, len(a)) {
+        fmt.Println("Equals!!!")
+    } else {
+        fmt.Println("NOT Equals!!!")
+    }
     fmt.Println("Insert Sort end")
 }
